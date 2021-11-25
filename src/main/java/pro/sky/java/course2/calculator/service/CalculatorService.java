1ae2273f6dc5fffa0d95f1bb9dcd279209f58643
dEfy310 <musinov_2004@mail.ru>
2021-11-25 21:15:12 +0300

    Homework3

---
+++ src/main/java/pro/sky/java/course2/calculator/service/CalculatorService.java
@@ -0,0 +1,8 @@
+package pro.sky.java.course2.calculator.service;
+
+public interface CalculatorService {
+    double sum(double num1, double num2);
+    double difference(double num1, double num2);
+    double multiply(double num1, double num2);
+    double divide(double num1, double num2);
+}
---
+++ src/main/java/pro/sky/java/course2/calculator/service/CalculatorServiceImpl.java
@@ -0,0 +1,29 @@
+package pro.sky.java.course2.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService{
+    @Override
+    public double sum(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public double difference(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public double divide(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Нельзя делить на 0!!!");
+        }
+        return num1/num2;
+    }
+
+    @Override
+    public double multiply(double num1, double num2) {
+        return num1*num2;
+    }
+}
